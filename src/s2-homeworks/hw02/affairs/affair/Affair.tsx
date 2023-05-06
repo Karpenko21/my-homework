a25b92b33e91b2d54f3eafe2851a1ed7d55d7304
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-05-06 10:04:19 +0300

    hw2 part 1

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { AffairType } from '../../HW2'
+import {AffairType} from '../../HW2'
 import s from './Affair.module.css'
 import s2 from '../Affairs.module.css'
 
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: any // need to fix any
+    deleteAffairCallback: (_id: number) => void// need to fix any
 }
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
-        // need to fix
+        props.deleteAffairCallback(props.affair._id)// need to fix
     }
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
@@ -35,6 +35,8 @@ function Affair(props: AffairPropsType) {
             <button
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
+                onClick={() => deleteCallback()}
+
                 // need to fix
 
             >
