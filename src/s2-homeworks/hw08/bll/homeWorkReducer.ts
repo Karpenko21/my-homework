ffe62ad1ab0ab85b0fa2b03a6ca108573b65d0c4
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-06-20 11:36:06 +0600

    hw8 part 2

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,16 +7,18 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            return state.sort((a, b) => a.name.localeCompare(b.name)) // need to fix
-            /*if (action.payload === 'up') {
-
-            } else if (action.payload === 'down') {
-                return state.sort((a, b) => b.name.localeCompare(a.name)) // need to fix
-            }*/
+            switch (action.payload) {
+                case "up" : {
+                    return state.sort((a, b) => a.name.localeCompare(b.name)) // need to fix
+                }
+                case "down": {
+                    return state.sort((a, b) => b.name.localeCompare(a.name)) // need to fix
+                }
+            }
         }
-        case 'check': {
 
-            return state // need to fix
+        case 'check': {
+            return state.filter(u => u.age >=18) // need to fix
         }
         default:
             return state
